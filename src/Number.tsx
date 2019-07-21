712cf27803c506fc3ef3afc76b0a2406b53487d4
13akstjq <13akstjq@naver.com>
2019-07-22 02:58:05 +0900

    add styled component typescript

--- src/Number.tsx
+++ src/Number.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.span``;
+const Container = styled.span<{ isRed: boolean }>`
+  color: ${props => (props.isRed ? "Red" : "Green")};
+`;
 
 interface IProps {
   number: Number;
 }
 
 const Number: React.FunctionComponent<IProps> = ({ number }) => (
-  <Container>{number}</Container>
+  <Container isRed={number >= 10 ? true : false}>{number}</Container>
 );
 
 export default Number;
